9d158e8672247a8375a61c85e74fd29da388968f
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-08-15 19:05:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![](https://images.unsplash.com/photo-1599889959407-598566c6e1f1?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80)
---
+++ mix.exs
@@ -0,0 +1,125 @@
+defmodule ExlaPrecompiled.MixProject do
+  use Mix.Project
+
+  def project do
+    if Mix.env() == :prod do
+      init()
+    end
+
+    [
+      app: :exla_precompiled,
+      version: "0.1.0-dev",
+      elixir: "~> 1.12",
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  defp deps do
+    [
+      {:exla, "~> 0.1.0-dev", github: "elixir-nx/nx", sparse: "exla", only: [:dev]},
+      {:nx, "~> 0.1.0-dev", github: "elixir-nx/nx", sparse: "nx", override: true, only: [:dev]}
+    ]
+  end
+
+  defp aliases do
+    [
+      local_exla_sha: &local_exla_sha/1
+    ]
+  end
+
+  defp local_exla_sha(_) do
+    IO.puts(exla_sha_in_project(__DIR__))
+  end
+
+  defp exla_sha_in_project(project_path) do
+    lock_path = Path.expand("mix.lock", project_path)
+
+    {:%{}, _, entries_ast} =
+      lock_path
+      |> File.read!()
+      |> Code.string_to_quoted!(warn_on_unnecessary_quotes: false)
+
+    {:{}, _, [:git, _url, sha | _]} = entries_ast[:exla]
+
+    sha
+  end
+
+  defp init() do
+    root_project_path = Path.expand("../..", __DIR__)
+    deps_path = Path.join(root_project_path, "deps")
+    exla_path = Path.join(deps_path, "exla/exla")
+    libexla_path = Path.join([exla_path, "priv", nif_filename()])
+
+    if File.exists?(exla_path) and not File.exists?(libexla_path) do
+      sha = exla_sha_in_project(root_project_path)
+      tag = "sha/" <> sha
+
+      Mix.shell("No exla binary found locally, trying find one online...")
+
+      case download_binary(tag, libexla_path) do
+        :ok ->
+          Mix.shell().info("Successfully downloaded precompiled exla binary!")
+          Mix.shell().info("Altering exla Makefile to avoid regular compilation")
+
+          # Replace Makefile, so that a regular compilation doesn't proceed
+          for makefile_name <- ["Makefile", "Makefile.win"] do
+            makefile_path = Path.join(exla_path, makefile_name)
+            File.rename!(makefile_path, makefile_path <> ".original")
+            # Keep an empty Makefile, so that elixir_make doesn't error out
+            File.write!(makefile_path, "noop: ;")
+          end
+
+        :error ->
+          Mix.shell().info(
+            "Couldn't find a matching precompiled exla binary, will need to compile"
+          )
+      end
+    end
+  end
+
+  defp download_binary(tag, destination_path) do
+    repo = "jonatanklosko/exla_precompiled"
+    url = "https://github.com/#{repo}/releases/download/#{tag}/#{nif_filename_with_target()}"
+    File.mkdir_p!(Path.dirname(destination_path))
+    download(url, destination_path)
+  end
+
+  defp download(url, dest) do
+    case Mix.shell().cmd("curl --fail -L #{url} > #{dest}") do
+      0 -> :ok
+      _ -> :error
+    end
+  end
+
+  defp nif_filename() do
+    "libexla-.#{nif_ext()}"
+  end
+
+  defp nif_filename_with_target() do
+    "libexla-#{target()}.#{nif_ext()}"
+  end
+
+  defp nif_ext() do
+    case :os.type() do
+      {:unix, _} -> "so"
+      {:win32, _} -> "dll"
+    end
+  end
+
+  defp target() do
+    case :string.split(:erlang.system_info(:system_architecture), '-', :all) do
+      [cpu, _vendor, os | _] ->
+        os = if List.starts_with?(os, 'darwin'), do: 'darwin', else: os
+        cpu = if os == 'darwin' and List.starts_with?(cpu, 'arm'), do: 'aarch64', else: cpu
+        "#{cpu}-#{os}"
+
+      ['win32'] ->
+        "x86_64-windows"
+    end
+  end
+end
